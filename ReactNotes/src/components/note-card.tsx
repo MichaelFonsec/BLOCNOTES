f019a678cc4877baba110e760b567f508fddd914
MichaelFonsec <jacquelinemartins743@gmail.com>
2024-05-14 17:34:37 -0300

    Refined scrollbar customization in NoteCard

--- ReactNotes/src/components/note-card.tsx
+++ ReactNotes/src/components/note-card.tsx
@@ -39,37 +39,40 @@ export function NoteCard({ nota, onNoteDeleted, onNoteEdited }: NoteCardProps) {
       </Dialog.Trigger>
       <Dialog.Portal>
         <Dialog.Overlay className="inset-0 fixed bg-black/50">
-          <Dialog.Content className="fixed overflow-hidden md:left-1/2 md:top-1/2 md:-translate-x-1/2 md:-translate-y-1/2 md:max-w-[640px] w-full md:h-[60vh] bg-slate-700 md:rounded-md flex flex-col outline-none">
-            <Dialog.Close className="absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100">
-              <X className="size-5" />
-            </Dialog.Close>
-            {isEditing ? (
-              <div className="flex flex-1 flex-col gap-3 p-5 w-full">
-                <textarea
-                  className="text-sm leading-6 text-slate-400 bg-transparent outline-none resize-none"
-                  value={editedContent}
-                  onChange={(e) => setEditedContent(e.target.value)}
-                />
-                <div className="flex flex-1 items-end gap-3">
-                  <button
-                    type="button"
-                    onClick={handleSave}
-                    className="w-full bg-lime-600 py-4  rounded-md  text-center text-sm text-slate-300 outline-none font-medium group hover:bg-lime-700 focus:outline-none focus-visible:ring-lime-400"
-                  >
-                    Salvar Edição
-                  </button>
-                  <button
-                    type="button"
-                    onClick={() => setIsEditing(false)}
-                    className="flex items-center justify-center w-full bg-slate-800 py-4 text-white rounded-md text-sm shadow-md outline-none font-medium hover:bg-slate-600 focus:outline-none focus-visible:ring-slate-400"
-                  >
-                    <Undo2 className="md:w-5 md:h-5 mr-2" />
-                    <span className="text-red-400 group-hover:underline">
-                      Cancelar Edição
-                    </span>
-                  </button>
-                </div>
-              </div>
+        <Dialog.Content className="fixed overflow-hidden md:left-1/2 md:top-1/2 md:-translate-x-1/2 md:-translate-y-1/2 md:max-w-[640px] w-full md:h-[60vh] bg-slate-700 md:rounded-md flex flex-col outline-none">
+      <Dialog.Close className="absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100">
+        <X className="size-5" />
+      </Dialog.Close>
+      {isEditing ? (
+        <div className="flex flex-1 flex-col gap-3 p-5 w-full">
+          <textarea
+            className="text-sm leading-6 py-7 h-48 text-slate-400 bg-transparent outline-none resize-none overflow-y-auto scrollbar-thin scrollbar-thumb-gray-300 scrollbar-track-gray-100"
+            value={editedContent}
+            onChange={(e) => setEditedContent(e.target.value)}
+            style={{ scrollbarWidth: 'none' }}
+          />
+          
+          <div className="flex flex-1 items-end gap-3">
+            <button
+              type="button"
+              onClick={handleSave}
+              className="w-full bg-lime-600 py-4 rounded-md text-center text-sm text-slate-300 outline-none font-medium group hover:bg-lime-700 focus:outline-none focus-visible:ring-lime-400"
+            >
+              Salvar Edição
+            </button>
+            <button
+              type="button"
+              onClick={() => setIsEditing(false)}
+              className="flex items-center justify-center w-full bg-slate-800 py-4 text-white rounded-md text-sm shadow-md outline-none font-medium hover:bg-slate-600 focus:outline-none focus-visible:ring-slate-400"
+            >
+              <Undo2 className="md:w-5 md:h-5 mr-2" />
+              <span className="text-red-400 group-hover:underline">
+                Cancelar Edição
+              </span>
+            </button>
+          </div>
+        </div>
+            
             ) : (
               <div className=" md:flex md:flex-1 md:flex-col md:gap-3 p-9 ">
                 <span className="text-sm font-medium text-slate-300">
